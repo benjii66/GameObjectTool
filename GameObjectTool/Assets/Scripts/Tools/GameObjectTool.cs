5c2e386fac15b568011a8585066b0bf253ee3336
B.N.J <benjamin51.simon@gmail.com>
2020-10-19 16:22:53 +0200

    Implement the ItemMenu

---
+++ GameObjectTool/Assets/Scripts/Tools/GameObjectTool.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEditor;
+
+
+public class GameObjectTool
+{
+
+	[MenuItem("Fake GameObject/Create Empty")]
+	static void CreateEmpty()
+	{
+		GameObject _truc = new GameObject("Empty");
+		Selection.activeGameObject = _truc.gameObject;
+	}
+
+	[MenuItem("Fake GameObject/3D Object/Cube")]
+	static void CreateSth()
+	{
+		GameObject _truc = new GameObject("Cube");
+		Selection.activeGameObject = _truc.gameObject;
+	}
+	[MenuItem("Fake GameObject/3D Object/Quad")]
+	static void CreateQuad()
+	{
+		GameObject _truc = new GameObject("Quad");
+		Selection.activeGameObject = _truc.gameObject;
+	}
+
+
+
+}
+
